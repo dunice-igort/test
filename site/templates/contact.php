1c6753bf139385b337c312ae890a8fdbd864b05a
Ivan Afonchenko <afonchenko.ivan.dunice@gmail.com>
2013-08-20 20:34:24 +0400

    create work page and contact page

---
+++ site/templates/contact.php
@@ -0,0 +1,14 @@
+<?php snippet('header') ?>
+<?php snippet('menu') ?>
+<?php snippet('submenu') ?>
+
+<section class="content">
+
+  <article>
+  
+    <div>
+        Contact Info
+    </div>
+    
+</section>
+<?php snippet('footer') ?>
---
+++ site/templates/work.php
@@ -0,0 +1,14 @@
+<?php snippet('header') ?>
+<?php snippet('menu') ?>
+<?php snippet('submenu') ?>
+
+<section class="content">
+
+  <article>
+  
+    <div>
+        Work Info
+    </div>
+    
+</section>
+<?php snippet('footer') ?>
